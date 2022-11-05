62e57a5ce1a9be6d42547d2cb62c8b33d931606f
aledosreis <alessandrodreis@outlook.com>
2022-11-05 11:41:29 -0300

    aula 3

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -7,9 +7,10 @@ import {
 } from "@expo-google-fonts/roboto";
 
 import { Loading } from "./src/components/Loading";
-import { SignIn } from "./src/screens/SignIn";
+import { Pools } from "./src/screens/Pools";
 
 import { THEME } from "./src/styles/theme";
+import { AuthContextProvider } from "./src/context/AuthContext";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -20,12 +21,14 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      {fontsLoaded ? <Loading /> : <SignIn />}
+      <AuthContextProvider>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {fontsLoaded ? <Loading /> : <Pools />}
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
